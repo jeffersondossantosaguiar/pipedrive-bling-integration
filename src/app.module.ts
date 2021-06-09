08dc68268bad921672b6f0ed42f3230238ecc929
Jefferson <jeffersonsantos.a@gmail.com>
2021-06-09 20:26:58 -0300

    Configurando o Nest e os modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlingController } from './bling/bling.controller';
+import { PipedriveController } from './pipedrive/pipedrive.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController, PipedriveController, BlingController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
